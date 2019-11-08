1ba316c2b2546068a7bd426b827ce7202bcf18df
igor.sharov <i.sharov@mail.ru>
2019-11-08 11:44:06 +0300

    add history

--- use_functions.py
+++ use_functions.py
@@ -1,27 +1,34 @@
 def banks():
 
     account = 0
+    history = []
 
     while True:
         print('1. пополнение счета')
         print('2. покупка')
-        print('3. выход')
+        print('3. история покупок')
+        print('4. выход')
 
         choice = input('Выберите пункт меню:')
         if choice == '1':
-            payment = float(input('Введите сумму пополнения счета:'))
+            payment = int(input('Введите сумму пополнения счета:'))
             account += payment
             print('Баланс счета:', account)
 
         elif choice == '2':
-            price_product = float(input('Введите стоимость покупки:'))
+            price_product = int(input('Введите стоимость покупки:'))
             if price_product > account:
                 print('На счете не достаточно средств')
             else:
+                product = input('Введите название покупки:')
                 account -= price_product
+                history.append(f"Товар: {product}; Стоимость: {price_product}")
                 print('Баланс счета:', account)
 
         elif choice == '3':
+            print('История покупок:', history)
+
+        elif choice == '4':
             print('До свидания')
             break
         else:
